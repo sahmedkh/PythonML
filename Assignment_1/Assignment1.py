83c6e1891e2eb041cbb8d4cf6ef883fd8c98d634
sahmedkh <ahmedsmahmoodedu@gmail.com>
2023-11-04 16:33:52 +0300

    Modified the output

--- Assignment_1/Assignment1.py
+++ Assignment_1/Assignment1.py
@@ -1,15 +1,17 @@
-
 # Importing The necessary packages
 import pandas
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
-# Providing data from the csv file and dividing it
-df = pandas.read_csv("insurance.csv")
-
+# Providing data from the csv file and seperating features
+df = pandas.read_csv("C:/Users/ahmed/OneDrive/Documents/Code/PythonML/Assignment_1/insurance.csv")
 x = df[['age', 'sex', 'bmi', 'children', 'smoker', 'region']]
-one_hot_x = pandas.get_dummies(x, columns = ['sex', 'smoker', 'region'])
 y = df['charges']
+
+# Encoding categorical data to be numerical
+one_hot_x = pandas.get_dummies(x, columns = ['sex', 'smoker', 'region'])
+
+# Splitting the dataset into training and testing
 x_train, x_test, y_train, y_test = train_test_split(one_hot_x, y, test_size=0.3, random_state=0) 
 
 # Creating and fitting the model
@@ -18,15 +20,17 @@ model.fit(x_train, y_train)
 
 # Getting summary of the model
 r_sq = model.score(x_train, y_train)
-print("Model score: ", r_sq)
-print("Intercept: ", model.intercept_)
-print("Coefficients: ", model.coef_)
+print("\nModel Summary: ")
+print("Model score: ", r_sq.round(3))
+print("Intercept: ", model.intercept_.round(3))
+print("Coefficients: ", model.coef_.round(3))
 
-# Predicting new values using the testing subset
+# Predicting using the testing set
 y_pred = model.predict(x_test)
 y_pred = [round(num, 2) for num in y_pred]
-print("Predicted responses: ", y_pred)
+print("\nPredicted responses: ", y_pred)
 
-# Score for the testing subset
+# Score for the testing set
 r2_sq = model.score(x_test, y_test)
-print("The score for the testing subset: ", r2_sq)
+print("\nThe score for the testing subset: ", r2_sq.round(3))
+print("\n")
